4f7ab206f001675ecbe83d11301ad1426db500d7
Agishika307 <ishikaagrawal307@gmail.com>
2026-07-07 19:07:21 +0530

    change

--- src/services/gemini.js
+++ src/services/gemini.js
@@ -1,7 +1,7 @@
 import { GoogleGenAI } from "@google/genai";
 
 const ai = new GoogleGenAI({
-  apiKey: "AQ.Ab8RN6JYfPUoxC4dRo5rWl6H4YyXdXInnxkX6fiI4xqfevJ8lQ"
+  apiKey: "AQ.Ab8RN6LI1VOfP2eqbIporBdUl5FXKjKTdjkbebk8k5rYsZC_Ww"
 });
 
 export default ai;
