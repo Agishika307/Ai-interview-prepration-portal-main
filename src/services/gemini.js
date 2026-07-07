c2820cd3832c5e707e383f8b3eeab551ceada56f
Agishika307 <ishikaagrawal307@gmail.com>
2026-07-07 12:09:48 +0530

    Initial commit

--- src/services/gemini.js
+++ src/services/gemini.js
@@ -1,7 +1,7 @@
 import { GoogleGenAI } from "@google/genai";
 
 const ai = new GoogleGenAI({
-  apiKey: "AQ.Ab8RN6IQZ9OHbFASwOaBFxtEkgMRWumE3oX3ihkcAQ0jkoRDsA"
+  apiKey: "AQ.Ab8RN6J4WtLH2TdCj45EXaqxpdkKCWu2PiZPe8Ffq7xEgUYeyQ"
 });
 
 export default ai;
